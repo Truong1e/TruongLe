a3285880709796365a46602bafda62936297cb76
LeTruong <hieuluong@gmail.com>
2023-12-07 10:34:49 +0700

    Create Lecture.cs

---
+++ Models/Lecture.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leductruong2512.Models
+{
+    internal class Lecture
+    {
+    }
+}
